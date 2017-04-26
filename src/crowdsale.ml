99d5e62f15c1d7bed5d122de266cd0de8545c551
Vincent Bernardoff <vb@luminar.eu.org>
2017-04-26 19:05:41 +0200

    add spend_multisig

--- src/crowdsale.ml
+++ src/crowdsale.ml
@@ -11,16 +11,15 @@ open Blockexplorer_lwt
 open Util
 open Util.Cmdliner
 
-let payment_addr_of_pkh ~cfg ~testnet pkh =
+let script_and_payment_addr_of_pkh ~cfg ~testnet pkh =
   let scriptRedeem =
     Script.P2SH_multisig.scriptRedeem
       ~append_script:Script.Script.Opcode.[Data pkh ; Drop]
       ~threshold:cfg.Cfg.threshold cfg.pks in
   let version =
     Payment_address.(if testnet then Testnet_P2SH else P2SH) in
-  let payment_address =
-    Payment_address.of_script ~version (Script.of_script scriptRedeem) in
-  Option.map payment_address ~f:Payment_address.to_b58check
+  scriptRedeem,
+  Payment_address.of_script_exn ~version (Script.of_script scriptRedeem)
 
 let lookup_utxos loglevel cfg testnet pkhs =
   set_loglevel loglevel ;
@@ -28,8 +27,10 @@ let lookup_utxos loglevel cfg testnet pkhs =
     if List.is_empty pkhs then In_channel.(input_lines stdin) else pkhs in
   let run () =
     Lwt_log.debug_f "Found %d pkhs" (List.length pkhs) >>= fun () ->
-    let addrs = List.filter_map pkhs
-        ~f:(fun pkh -> payment_addr_of_pkh ~cfg ~testnet pkh) in
+    let addrs = List.map pkhs ~f:begin fun pkh ->
+        let _script, addr = script_and_payment_addr_of_pkh ~cfg ~testnet pkh in
+        Payment_address.to_b58check addr
+      end in
     List.iter2_exn pkhs addrs ~f:begin fun pkh (`Base58 addr) ->
       Lwt_log.ign_debug_f "%s -> %s" pkh addr
     end ;
@@ -113,26 +114,21 @@ let decode_script =
   Term.(const decode_script $ loglevel $ script),
   Term.info ~doc "decode-script"
 
+let input_and_script_of_utxo utxo = match utxo.Utxo.confirmed with
+  | Unconfirmed _ -> invalid_arg "input_of_utxo: unconfirmed"
+  | Confirmed { vout ; scriptPubKey } ->
+    let prev_out_hash = Hash.Hash32.of_hex_exn (`Hex utxo.Utxo.txid) in
+    let script = Script.invalid () in
+    Transaction.Input.create ~prev_out_hash ~prev_out_index:vout ~script (),
+    Option.value_exn (Script.of_hex (`Hex scriptPubKey))
+
 let spend_n loglevel cfg testnet privkey source dest_addrs amount fees =
   set_loglevel loglevel ;
-  let input_and_script_of_utxo utxo = match utxo.Utxo.confirmed with
-    | Unconfirmed _ -> invalid_arg "input_of_utxo: unconfirmed"
-    | Confirmed { vout ; scriptPubKey } ->
-      let prev_out_hash = Hash.Hash32.of_hex_exn (`Hex utxo.Utxo.txid) in
-      let script = Script.invalid () in
-      Transaction.Input.create ~prev_out_hash ~prev_out_index:vout ~script (),
-      Option.value_exn (Script.of_hex (`Hex scriptPubKey))
-  in
   let amount_of_utxos =
     List.fold_left ~init:0 ~f:(fun acc utxo -> acc + utxo.Utxo.amount) in
   let output_of_dest_addr addr ~value =
     let script = Payment_address.to_script addr in
     Transaction.Output.create ~script ~value in
-  let endorsement_of_input tx index prev_out_script secret =
-    match Transaction.Sign.endorse
-            ~prev_out_script ~tx ~index ~secret () with
-    | None -> invalid_arg "endorsement_of_input"
-    | Some endorsement -> endorsement in
   let dest_addrs = List.map dest_addrs ~f:Payment_address.of_b58check_exn in
   let privkey = Ec_private.of_wif_exn privkey in
   let pubkey = Ec_public.of_private privkey in
@@ -166,10 +162,11 @@ let spend_n loglevel cfg testnet privkey source dest_addrs amount fees =
         change_out :: List.map dest_addrs ~f:(output_of_dest_addr ~value:amount_per_addr) in
       let tx = Transaction.create inputs outputs in
       let scriptSigs =
-        List.mapi inputs_and_scripts ~f:begin fun i (input, script) ->
+        List.mapi inputs_and_scripts ~f:begin fun index (_input, prev_out_script) ->
           let open Transaction.Sign in
-          let chunk = endorsement_of_input tx i script secret in
-          Script.P2PKH.scriptSig chunk pubkey
+          let endorsement =
+            Transaction.Sign.endorse_exn ~tx ~index ~prev_out_script ~secret () in
+          Script.P2PKH.scriptSig endorsement pubkey
         end in
       List.iter2_exn inputs scriptSigs ~f:begin fun i e ->
         Transaction.Input.set_script i e
@@ -198,32 +195,19 @@ let spend_n =
   Term.(const spend_n $ loglevel $ cfg $ testnet $ privkey $ source $ dests $ amount $ fees),
   Term.info ~doc "spend-n"
 
-let spend_multisig loglevel cfg testnet privkey source dest =
+let spend_multisig loglevel cfg testnet (sk1, sk2) pkh dest =
+  let scriptRedeem, source = script_and_payment_addr_of_pkh ~cfg ~testnet pkh in
   set_loglevel loglevel ;
-  let input_and_script_of_utxo utxo = match utxo.Utxo.confirmed with
-    | Unconfirmed _ -> invalid_arg "input_of_utxo: unconfirmed"
-    | Confirmed { vout ; scriptPubKey } ->
-      let prev_out_hash = Hash.Hash32.of_hex_exn (`Hex utxo.Utxo.txid) in
-      let script = Script.invalid () in
-      Transaction.Input.create ~prev_out_hash ~prev_out_index:vout ~script (),
-      Option.value_exn (Script.of_hex (`Hex scriptPubKey))
-  in
   let amount_of_utxos =
     List.fold_left ~init:0 ~f:(fun acc utxo -> acc + utxo.Utxo.amount) in
   let output_of_dest_addr addr ~value =
     let script = Payment_address.to_script addr in
     Transaction.Output.create ~script ~value in
-  let endorsement_of_input tx index prev_out_script secret =
-    match Transaction.Sign.endorse
-            ~prev_out_script ~tx ~index ~secret () with
-    | None -> invalid_arg "endorsement_of_input"
-    | Some endorsement -> endorsement in
   let dest = Payment_address.of_b58check_exn dest in
-  let privkey = Ec_private.of_wif_exn privkey in
-  let pubkey = Ec_public.of_private privkey in
-  let secret = Ec_private.secret privkey in
+  let sk1, sk2 = Ec_private.(of_wif_exn sk1, of_wif_exn sk2) in
+  let secret1, secret2 = Ec_private.(secret sk1, secret sk2) in
   let run () =
-    utxos ~testnet [source] >>= function
+    utxos ~testnet [Payment_address.to_b58check source] >>= function
     | Error err -> Lwt_log.error (Http.string_of_error err)
     | Ok utxos ->
       let utxos = List.filter utxos ~f:begin fun utxo ->
@@ -243,10 +227,13 @@ let spend_multisig loglevel cfg testnet privkey source dest =
       let output = output_of_dest_addr dest ~value:total_spendable in
       let tx = Transaction.create inputs [output] in
       let scriptSigs =
-        List.mapi inputs_and_scripts ~f:begin fun i (input, script) ->
+        List.mapi inputs_and_scripts ~f:begin fun index (input, prev_out_script) ->
           let open Transaction.Sign in
-          let chunk = endorsement_of_input tx i script secret in
-          Script.P2PKH.scriptSig chunk pubkey
+          let ed1 =
+            Transaction.Sign.endorse_exn ~tx ~index ~prev_out_script ~secret:secret1 () in
+          let ed2 =
+            Transaction.Sign.endorse_exn ~tx ~index ~prev_out_script ~secret:secret2 () in
+          Script.P2SH_multisig.scriptSig ~endorsements:[ ed1 ; ed2 ] ~scriptRedeem
         end in
       List.iter2_exn inputs scriptSigs ~f:begin fun i e ->
         Transaction.Input.set_script i e
@@ -261,9 +248,9 @@ let spend_multisig loglevel cfg testnet privkey source dest =
 let spend_multisig =
   let doc = "Spend bitcoins from a multisig address." in
   let privkey =
-    Arg.(required & (pos 0 (some string) None) & info [] ~docv:"PRIVKEY") in
+    Arg.(required & (pos 0 (some (t2 string string)) None) & info [] ~docv:"SECRETKEYS") in
   let source =
-    Arg.(required & (pos 1 (some Conv.payment_addr) None) & info [] ~docv:"SOURCE") in
+    Arg.(required & (pos 1 (some Conv.hex) None) & info [] ~docv:"PKH") in
   let dest =
     Arg.(required & (pos 2 (some Conv.payment_addr) None) & info [] ~docv:"DEST") in
   Term.(const spend_multisig $ loglevel $ cfg $ testnet $ privkey $ source $ dest),
